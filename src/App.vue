5d8820720246feeff05260cc06039aa724f1f67d
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 22:53:12 +0200

    Ceando componentes1

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ContadorImpares />
+    <InputFields />
+  </div>
+</template>
+
+<script>
+import ContadorImpares from './components/ContadorImpares.vue';
+import InputFields from './components/InputFields.vue';
+
+export default {
+  components: {
+    ContadorImpares,
+    InputFields
+  }
+};
+</script>
