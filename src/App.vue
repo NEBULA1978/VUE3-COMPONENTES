e74b447b79dd272e10e09effe72ca31991dd005a
NEBULA1978 <rpascualey@gmail.com>
2023-08-10 22:58:44 +0200

    Ceando componentes3

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div>
     <ContadorImpares />
     <InputFields />
+    <Usuarios />
   </div>
 </template>
 
 <script>
 import ContadorImpares from './components/ContadorImpares.vue';
 import InputFields from './components/InputFields.vue';
+import Usuarios from './components/Usuarios.vue';
 
 export default {
   components: {
     ContadorImpares,
-    InputFields
+    InputFields,
+    Usuarios
   }
 };
 </script>
