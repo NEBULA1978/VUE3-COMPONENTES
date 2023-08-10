081dbfb61f8f004fe94ac1e19ed25a549cd9bd18
NEBULA1978 <rpascualey@gmail.com>
2023-08-11 00:00:38 +0200

    Fallo acomponente

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,30 @@
     <ContadorImpares />
     <InputFields />
     <Usuarios />
+    <TablaMultiplicar />
+    <VerificadorPalindromo /> <!-- Nuevo componente agregado -->
+    <VerificadorPalindromo2 /> <!-- Nuevo componente agregado -->
+    <buscarCoincidencias />
   </div>
 </template>
-
 <script>
 import ContadorImpares from './components/ContadorImpares.vue';
 import InputFields from './components/InputFields.vue';
 import Usuarios from './components/Usuarios.vue';
-
+import TablaMultiplicar from './components/TablaMultiplicar.vue';
+import VerificadorPalindromo from './components/VerificadorPalindromo.vue';
+import VerificadorPalindromo2 from './components/VerificadorPalindromo2.vue'; // Corregir el nombre del import
+import BuscarCoincidencia from './components/BuscarCoincidencia.vue';
 export default {
   components: {
     ContadorImpares,
     InputFields,
-    Usuarios
+    Usuarios,
+    TablaMultiplicar,
+    VerificadorPalindromo,
+    VerificadorPalindromo2, 
+    BuscarCoincidencia
   }
 };
 </script>
+
